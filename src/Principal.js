824fa262e166c10f9c0bccd13926524c284108d7
marcosjrs <marcosjrs80@gmail.com>
2017-11-01 19:12:25 +0100

    Esqueleto de Componente Principal y cambio en el index para que lo lance

---
+++ src/Principal.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Principal extends Component{
+    render(){
+        return (
+            <h1>Componente Principal</h1>
+        );
+    }
+}
+
+export default Principal;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './Counter';
-import App from './App';
+import Principal from './Principal';
 import './index.css';
 
 ReactDOM.render(
-  <App counter={new Counter()} />,
+  <Principal />,
   document.getElementById('root')
 );
